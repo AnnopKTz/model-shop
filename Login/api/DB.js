358ddec29e8ea7a59c37c58a3efabaa3526bff3f
AphisitKh <35342134+AphisitKh@users.noreply.github.com>
2019-10-20 22:47:32 +0700

    login
    login hashPassword And connect to MongoDB server

---
+++ Login/api/DB.js
@@ -0,0 +1,3 @@
+module.exports = {
+		DB: 'mongodb+srv://jo11:mongopass@store-8n3gy.azure.mongodb.net/model-shop_db'
+}
---
+++ Login/src/MessageBundle.js
@@ -0,0 +1,24 @@
+export const REGISTRATION_FIELDS = {
+	FIRST_NAME : 'First Name',
+	LAST_NAME : 'Last Name',
+	EMAIL_NAME: 'Email',
+	JOBTITLE_NAME: 'Job Title',
+	REGISTER: 'Register',
+	CANCEL: 'Cancel',
+	REGISTRATION_HEADING: 'Sign Up'
+}
+
+export const REGISTRATION_MESSAGE = 'Registered Successfully';
+export const ERROR_IN_REGISTRATION = 'Error in Registration';
+export const LOGIN_MESSAGE = 'Login Successful';
+export const ERROR_IN_LOGIN = 'Error in Login'
+
+export const COMMON_FIELDS = {
+	USER_NAME : 'ID',
+	PASSWORD : 'Password'
+}
+
+export const LOGIN_FIELDS = {
+	LOGIN: 'Login',
+	LOGIN_HEADING: 'WELCOME'
+}
